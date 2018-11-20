190edbf786c8677f6691695c6c5adf862242aa57
krec-kiran <krec.kiran@gmail.com>
2018-11-20 11:13:56 +0000

    new code!

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'pages/index.html.j2')
+    return render(request, 'pages/index.html')
 
 
 def about(request):
-    return render(request, 'pages/about.html.j2')
+    return render(request, 'pages/about.html')
